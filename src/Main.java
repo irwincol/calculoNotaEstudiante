3fc21df2c3f0c3b2fe65b7f112dfdcb28298b6d8
irwincolmenarez@gmail.com <irwincol>
2025-04-09 21:14:17 -0500

    se debe terminar los swit de cada uno de los programas

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +11,7 @@ public class Main {
          gastronomia: costos, panaderia dulce, cocina internacional)
 
         El calculo de nota es de la siguiente manera:
-        para cada materia hau un examen parcial que equivale al 20% de la nota
+        para cada materia hay un examen parcial que equivale al 20% de la nota
         un taller que equivale el 30% de la nota y
         un examen final que equivale al 50% de la nota)
 
@@ -20,7 +21,45 @@ public class Main {
         Si la nota es igual o superior a 3, asi que le queda la materia
         Si el estudiante hace plan de mejoramiento, la nota maxima de la materia sera 3
 
-        El sistema debe discriminar
+        El sistema debe discriminar debe discriminar la informacion general del estudiante: nombre, documento, nota materia
          */
+        Scanner sc = new Scanner(System.in);
+
+        /*Definicion y asignacion de variables */
+
+        System.out.print("Ingrese el nombre del estudiante");
+        String nombreEstudiante = sc.nextLine();
+        System.out.println("Ingrese el documento del estudiante");
+        String documentoEstudiante = sc.nextLine();
+        System.out.println("Seleccione el programa: \n1 - Desarrollo de software\n2 - Diseño Grafico\n3 - Gastronomia");
+        String programa = sc.nextLine();
+        switch (programa){
+            case "1":
+                System.out.println("Programa de desarrollo de Software");
+                System.out.println("Seleccione la materia:\n1 - Lógica de programacion\n2 - Metodologias agiles\n3 - Introduccion a la progrmacion");
+                String materia = sc.nextLine();
+                switch (materia){
+                    case "1":
+                        System.out.println("nota de logica de programacion");
+                        break;
+                    case "2":
+                        System.out.println("nota de metodologias agiles: ");
+                        break;
+                    case "3":
+                        System.out.println("nota de introduccion a la programacion");
+                        break;
+                }
+                break;
+            case "2":
+                System.out.println("Programa de Diseño Grafico");
+                break;
+            case "3":
+                System.out.println("Programa de Gastronomia");
+                break;
+        }
+
+
+
+
     }
 }
